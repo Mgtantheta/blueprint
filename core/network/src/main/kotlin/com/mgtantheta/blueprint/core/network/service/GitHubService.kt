14db02ebb81b578345eaf4689fbb3260e738bb61
h-muraki <freecitynj@gmail.com>
2026-01-07 00:11:34 +0900

    feat: refactor GitHubService to map API response to domain model

--- core/network/src/main/kotlin/com/mgtantheta/blueprint/core/network/service/GitHubService.kt
+++ core/network/src/main/kotlin/com/mgtantheta/blueprint/core/network/service/GitHubService.kt
@@ -1,5 +1,6 @@
 package com.mgtantheta.blueprint.core.network.service
 
+import com.mgtantheta.blueprint.core.model.Repo
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
@@ -8,14 +9,18 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Serializable
-data class GitHubRepo(
+private data class GitHubRepoResponse(
     val name: String,
-)
+){
+    fun toDomain(): Repo = Repo(name = name)
+}
 
 @Singleton
 class GitHubService @Inject constructor(
     private val httpClient: HttpClient,
 ) {
-    suspend fun getRepos(username: String): List<GitHubRepo> =
-        httpClient.get("https://api.github.com/users/$username/repos").body()
+    suspend fun getRepos(username: String): List<Repo> =
+        httpClient.get("https://api.github.com/users/$username/repos")
+            .body<List<GitHubRepoResponse>>()
+            .map { it.toDomain() }
 }
